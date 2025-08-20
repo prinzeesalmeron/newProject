c1ac5f466e857e63d420c5ba7bc91bf5e09879ab
prinzeesalmeron <prinzeesalmeron@gmail.com>
2025-08-20 11:00:02 +0800

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -93,19 +93,7 @@ export const Navbar = () => {
             )}
           </div>
 
-          {/* Search Bar */}
-          <div className="hidden md:flex items-center max-w-md flex-1 mx-8">
-            <div className="relative w-full">
-              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400 dark:text-gray-500" />
-              <input
-                type="text"
-                placeholder="Search properties..."
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-                className="w-full pl-10 pr-4 py-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-white dark:bg-gray-700 text-gray-900 dark:text-white placeholder-gray-500 dark:placeholder-gray-400"
-              />
-            </div>
-          </div>
+          
 
           {/* User Authentication */}
           <div className="flex items-center space-x-4">

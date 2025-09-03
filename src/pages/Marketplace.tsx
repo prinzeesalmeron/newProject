906a2ffd519617f30340e08db1449d0c442c75e5
prinzeesalmeron <prinzeesalmeron@gmail.com>
2025-09-03 14:19:34 +0800

    Updated Marketplace.tsx

--- src/pages/Marketplace.tsx
+++ src/pages/Marketplace.tsx
@@ -182,7 +182,7 @@ export const Marketplace = () => {
               className="flex items-center space-x-2 px-4 py-2 rounded-full bg-gray-100 dark:bg-gray-700 text-gray-700 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-600 transition-colors"
             >
               <Filter className="h-4 w-4" />
-              <span>Advanced Filters</span>
+              <span> Filter</span>
             </button>
           </div>
         </div>
